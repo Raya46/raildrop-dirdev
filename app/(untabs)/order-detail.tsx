91d889118b981397ace0411451afa7780427b6b8
Raya46 <muhammadrayaarrizki@gmail.com>
2025-08-06 18:55:26 +0700

    order-detail

---
+++ app/(untabs)/order-detail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const OrderDetail = () => {
+  return (
+    <View>
+      <Text>OrderDetail</Text>
+    </View>
+  );
+};
+
+export default OrderDetail;
