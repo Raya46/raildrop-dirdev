ab2f1b6bcb860f6ec85cb65ad2b2c511917c248e
Raya46 <muhammadrayaarrizki@gmail.com>
2025-08-06 08:42:17 +0700

    login, register, tabs, package

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>Login</Text>
+    <View className="flex-1 flex flex-col mx-4 justify-center">
+      <Text className="text-3xl font-bold text-center">Welcome Back</Text>
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+        placeholder="Email"
+      />
+      <TextInput
+        onChangeText={(text) => setPassword(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] my-4"
+        placeholder="Password"
+      />
+      <TouchableOpacity
+        className="rounded-full w-full bg-blue-800 p-4"
+        onPress={() => router.push("/home")}
+      >
+        <Text className="text-white text-center text-xl">Login</Text>
+      </TouchableOpacity>
+      <View className="flex flex-row gap-1 items-center justify-center mt-4">
+        <Text>Don{"t"} Have an Account?</Text>
+        <Link href={"/register"} className="text-[#004C98]">
+          Sign Up Now!
+        </Link>
+      </View>
     </View>
   );
 };
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,10 +1,47 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
   return (
-    <View>
-      <Text>Register</Text>
+    <View className="flex flex-col flex-1 justify-center mx-4">
+      <Text className="text-3xl font-bold text-center">Sign Up</Text>
+      <TextInput
+        onChangeText={(text) => setName(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+        placeholder="Name"
+      />
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+        placeholder="Email"
+      />
+      <TextInput
+        onChangeText={(text) => setPassword(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+        placeholder="Password"
+      />
+      <TextInput
+        onChangeText={(text) => setConfirmPassword(text)}
+        className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+        placeholder="Confirm Password"
+      />
+      <TouchableOpacity
+        className="rounded-full w-full bg-blue-800 p-4 mb-4"
+        onPress={() => router.push("/home")}
+      >
+        <Text className="text-white text-center text-xl">Login</Text>
+      </TouchableOpacity>
+      <View className="flex flex-row gap-1 items-center justify-center mt-4">
+        <Text>Already Have an Account?</Text>
+        <Link href={"/"} className="text-[#004C98]">
+          Login Now!
+        </Link>
+      </View>
     </View>
   );
 };
