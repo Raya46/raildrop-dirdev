9ec777d97ba92344a097fec568d599e3b9b9616b
Faditya <daffaaditya2607@gmail.com>
2025-08-06 15:19:02 +0700

    feat: onboarding 90%

--- app/(onboarding)/step2.tsx
+++ app/(onboarding)/step2.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const Step2 = () => {
+  const [note, setNote] = useState("");
+  const [labelAddress, setLabelAddress] = useState("");
+  const [fullAddress, setFullAddress] = useState("");
   return (
-    <View>
-      <Text>Step2</Text>
+    <View className="flex-1 flex flex-col justify-center mx-4">
+        <View className="flex justify-center ml-8">
+            <Image
+                source={require("@/assets/images/onboarding.png")}
+                className="w-[300px] h-[300px]"
+            />
+        </View>
+        <Text className="text-3xl font-bold text-center mt-4">
+            Enter Your Address :{" "}
+        </Text>
+        <TextInput
+            onChangeText={(text) => setFullAddress(text)}
+            className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+            placeholder="Full Address"
+        />
+        <TextInput
+            onChangeText={(text) => setNote(text)}
+            className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+            placeholder="Note"
+        />
+        <TextInput
+            onChangeText={(text) => setLabelAddress(text)}
+            className="p-4 rounded-md bg-[#D9E4F0] mt-4"
+            placeholder="Label Address"
+        />
+        
+        <View className="flex mt-4 justify-center mx-4 ">
+               
+            <Text className="opacity-60">By clicking the button below, you acknowledge that you have read and agreed to the terms and conditions.</Text>
+        </View>
+
+        <TouchableOpacity
+            className="rounded-full w-full bg-blue-800 p-4 mt-4"
+            onPress={() => router.push("/(onboarding)/step3")}
+        >
+            <Text className="text-white text-center text-semibold text-xl">
+            Submit
+            </Text>
+        </TouchableOpacity>
     </View>
   );
 };
--- app/(onboarding)/step3.tsx
+++ app/(onboarding)/step3.tsx
@@ -2,8 +2,7 @@ import { router } from "expo-router";
 import React, { useState } from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-const Step1 = () => {
-  const [phone, setPhone] = useState("");
+const Step3 = () => {
   return (
     <View className="flex-1 flex flex-col justify-center mx-4">
         <View className="flex justify-center ml-8">
@@ -12,17 +11,12 @@ const Step1 = () => {
                 className="w-[300px] h-[300px]"
             />
         </View>
-        <Text className="text-3xl font-bold text-left mt-4">
-            Enter Your Phone Number:{" "}
-        </Text>
-        <TextInput
-            onChangeText={(text) => setPhone(text)}
-            className="p-4 rounded-md bg-[#D9E4F0] mt-4"
-            placeholder="Phone Number"
-        />
+        
+
+
         <TouchableOpacity
             className="rounded-full w-full bg-blue-800 p-4 mt-4"
-            onPress={() => router.push("/(onboarding)/step2")}
+            onPress={() => router.push("/(onboarding)/step4")}
         >
             <Text className="text-white text-center text-semibold text-xl">
             Submit
@@ -32,4 +26,4 @@ const Step1 = () => {
   );
 };
 
-export default Step1;
+export default Step3;
